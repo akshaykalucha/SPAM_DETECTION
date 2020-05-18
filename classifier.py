79f31133ac07f7cd7dfed991ac07208b9f3140b2
akshay kalucha <akshaykalucha@gmail.com>
2020-05-18 23:58:03 +0530

    minor updates

--- classifier.py
+++ classifier.py
@@ -74,8 +74,8 @@ def extract_features(mail_dir):
 
 
 
-TRAIN_DIR = [FOLDER_PATH]
-TEST_DIR = [FOLDER_PATH]
+TRAIN_DIR = 'E://CodingProjects//SPAMSVM//train-mails'
+TEST_DIR = 'E://CodingProjects//SPAMSVM//test-mails'
 
 dictionary = make_Dictionary(TRAIN_DIR)
 
@@ -83,20 +83,20 @@ print("reading and processing emails from file.")
 
 # features_matrix, train_labels = extract_features(TRAIN_DIR)
 # test_feature_matrix, test_labels = extract_features(TEST_DIR)
-features_matrix = load([FILE_PATH])
-train_labels = load([FILE_PATH])
-test_feature_matrix = load([FILE_PATH])
-test_labels = load([FILE_PATH])
+features_matrix = load("E://CodingProjects//SPAMSVM//code//temp//features_matrix.txt")
+train_labels = load("E://CodingProjects//SPAMSVM//code//temp//train_labels.txt")
+test_feature_matrix = load("E://CodingProjects//SPAMSVM//code//temp//test_feature_matrix.txt")
+test_labels = load("E://CodingProjects//SPAMSVM//code//temp//test_labels.txt")
 
 # features_matrix = features_matrix[:len(features_matrix)//10]
 # train_labels = train_labels[:len(train_labels)//10]
 
 print(train_labels, 'newww')
 
-save([FILE_PATH], features_matrix)
-save([FILE_PATH], train_labels)
-save([FILE_PATH], test_feature_matrix)
-save([FILE_PATH], test_labels)
+save("E://CodingProjects//SPAMSVM//code//temp//features_matrix.txt", features_matrix)
+save("E://CodingProjects//SPAMSVM//code//temp//train_labels.txt", train_labels)
+save("E://CodingProjects//SPAMSVM//code//temp//test_feature_matrix.txt", test_feature_matrix)
+save("E://CodingProjects//SPAMSVM//code//temp//test_labels.txt", test_labels)
 
 model = svm.SVC(kernel="rbf", C = 1)
 print("Training model.")
