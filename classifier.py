191c346169b85406a8fdcc0acb1a09c998283d26
akshay kalucha <akshaykalucha@gmail.com>
2020-06-02 23:02:06 +0530

    updates

--- classifier.py
+++ classifier.py
@@ -25,20 +25,20 @@ def save(file_name, model):
 def make_Dictionary(root_dir):
     all_words = []
     emails = [os.path.join(root_dir,f) for f in os.listdir(root_dir)]
-    # for mail in emails:
-    #     with open(mail) as m:
-    #         for line in m:
-    #             words = line.split()
-    #             all_words += words
-    # dictionary = Counter(all_words)
-    # list_to_remove = list(dictionary)
+    for mail in emails:
+        with open(mail) as m:
+            for line in m:
+                words = line.split()
+                all_words += words
+    dictionary = Counter(all_words)
+    list_to_remove = list(dictionary)
 
-    # for item in list_to_remove:
-    #     if item.isalpha() == False:
-    #         del dictionary[item]
-    #     elif len(item) == 1:
-    #         del dictionary[item]
-    # dictionary = dictionary.most_common(3000)
+    for item in list_to_remove:
+        if item.isalpha() == False:
+            del dictionary[item]
+        elif len(item) == 1:
+            del dictionary[item]
+    dictionary = dictionary.most_common(3000)
 
     return dictionary
 
